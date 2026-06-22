73d31954c21c6c5c7acf0db9c15413d257543882
Fan Liu <liuf4491@gmail.com>
2026-06-22 15:24:23 +0000

    restore robotwin dataloader workers

--- wan_va/configs/va_robotwin_lingbot_train_cfg.py
+++ wan_va/configs/va_robotwin_lingbot_train_cfg.py
@@ -35,4 +35,4 @@ va_robotwin_lingbot_train_cfg.save_root = os.getenv(
 
 va_robotwin_lingbot_train_cfg.enable_wandb = _env_bool("ENABLE_WANDB", False)
 va_robotwin_lingbot_train_cfg.save_interval = 50000
-va_robotwin_lingbot_train_cfg.load_worker = 4
+va_robotwin_lingbot_train_cfg.load_worker = 16
